70f7a2c352359e11283ef2a6b11d0a75d8137676
Alice Ying <acying@umich.edu>
2020-03-07 00:23:17 -0500

    Initial commit.

---
+++ FitnessApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FitnessApp
+//
+//  Created by Alice Ying on 3/7/20.
+//  Copyright © 2020 Alice Ying. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
